f003e4b92c80682cdc293f0c0109f362c808baa5
Eugene <yjinj13@gmail.com>
2021-03-02 17:12:31 +0900

    modification_1

--- app.py
+++ app.py
@@ -38,5 +38,13 @@ def show_order():
     orders = list(db.shopping.find({}, {'_id': False}))
     return jsonify({'all_orders':orders})
 
+# delete
+@app.route('/shopping/delete', methods=['POST'])
+def delete_order():
+    name_receive = request.form['name_give']
+
+    db.shopping.delete_one({'name':name_receive})
+    return jsonify({'msg': '삭제 완료'})
+
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
